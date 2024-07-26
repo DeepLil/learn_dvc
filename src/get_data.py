5ba7a1a1407408846724ec1669f12ed1f0b305d7
DeepLil <phanisandeep1994@gmail.com>
2024-07-27 02:15:50 +0530

    stage1 completed

--- src/get_data.py
+++ src/get_data.py
@@ -14,11 +14,10 @@ def read_params(config_path):
 
 def get_data(config_path):
     config = read_params(config_path)
-    print(config)
     data_path = config["data_source"]["s3_source"]
     df = pd.read_csv(data_path, sep=",", encoding= 'utf-8')
     return df
-
+#extra comments
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
