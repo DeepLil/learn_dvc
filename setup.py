55e4f2d3e2ee12a308cb25223b6c53e4a8debaea
DeepLil <phanisandeep1994@gmail.com>
2024-07-28 23:07:20 +0530

    testing using pytest

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src", version = "0.0.1",
+    description = 'its a wineq package',
+    author = 'DeepLil',
+    packages = find_packages(),
+    license = "MIT"
+)
+
